91754445cec61d26458b815922e33a73bdbece50
Matthew Moyer <matthewrmoyer@gmail.com>
2017-02-28 16:39:52 -0700

    refactor readfilesync to readfile

--- pets.js
+++ pets.js
@@ -13,21 +13,42 @@ if (process.argv[2] == 'read') {
 }
 
 function read() {
-	var file = './pets.json'
-	var contents = fs.readFileSync(file).toString();
-	var parsedContents = JSON.parse(contents);
+	//????if(err){throw err;}
+	// var file = './pets.json'
+	// var contents = fs.readFileSync(file).toString();
+	// var parsedContents = JSON.parse(contents);
 
-	if (!process.argv[3]) {
-		console.log(parsedContents)
-	}
-	if (process.argv[3]) {
-		if (process.argv[3] > parsedContents.length) {
-			console.log('Usage: node pets.js read INDEX')
-		} else {
-			var targetObject = parsedContents[process.argv[3]]
-			console.log(targetObject)
+	// if (!process.argv[3]) {
+	// 	console.log(parsedContents)
+	// }
+	// if (process.argv[3]) {
+	// 	if (process.argv[3] >= parsedContents.length) {
+	// 		console.log('Usage: node pets.js read INDEX')
+	// 	} else {
+	// 		var targetObject = parsedContents[process.argv[3]]
+	// 		console.log(targetObject)
+	// 	}
+	// }
+	var filePath = './pets.json'
+
+	fs.readFile(filePath, 'utf8', (err, data) => {
+		if (err) {
+			throw err
+		};
+
+		var parsedData = JSON.parse(data)
+
+		if(!process.argv[3]){console.log(parsedData)}
+
+		if (process.argv[3]) {
+			var targetIndex = process.argv[3];
+			if (targetIndex >= parsedData.length) {
+				console.log('Usage: node pets.js read INDEX')
+			} else {
+				console.log(parsedData[targetIndex])
+			}
 		}
-	}
+	})
 }
 
 function create() {
