aa81f4e185c8a4d36f2d59f09a5bf7eda8fc63ba
Matthew Moyer <matthewrmoyer@gmail.com>
2017-02-28 15:23:10 -0700

    connect crud methods

--- pets.js
+++ pets.js
@@ -1,1 +1,28 @@
-console.log('pets.js')
+
+if (process.argv[2] == 'read') {
+	read()
+} else if (process.argv[2] == 'create') {
+	create()
+} else if (process.argv[2] == 'update') {
+	update()
+} else if (process.argv[2] == 'destroy') {
+	destroy()
+} else {
+	console.log("Usage: node pets.js [read | create | update | destroy")
+}
+
+function read() {
+	console.log('reading')
+}
+
+function create() {
+	console.log('creating')
+}
+
+function update() {
+	console.log('updating')
+}
+
+function destroy(){
+	console.log('destroying')
+}
