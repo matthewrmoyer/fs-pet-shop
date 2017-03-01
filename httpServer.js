9c2f91639a957ad5ae2b587d0b9726d07c2e23af
Matthew Moyer <matthewrmoyer@gmail.com>
2017-03-01 15:29:56 -0700

    format

--- httpServer.js
+++ httpServer.js
@@ -2,19 +2,14 @@ const http = require('http')
 const PORT = 8080
 const pets = require('./pets.json')
 
-
 var server = http.createServer(function(req, res) {
 	const index = req.url.split("/").pop();
 	console.log("Server is listening at " + index)
-
-	// res.end(JSON.stringify(pets));
-	// 		res.end(JSON.stringify(pets[0]));
 	if (index == "pets") {
 		res.writeHead(200, {
 			"Content-Type": "application/json"
 		})
 		res.end(JSON.stringify(pets));
-
 	} else if (index < 0) {
 		res.writeHead(404, {
 			"Content-Type": "text/plain"
@@ -27,7 +22,6 @@ var server = http.createServer(function(req, res) {
 		});
 		res.write("Not Found");
 		res.end();
-
 	} else if (index == "1") {
 		res.writeHead(200, {
 			"Content-Type": "application/json"
@@ -44,13 +38,10 @@ var server = http.createServer(function(req, res) {
 		})
 		res.end(JSON.stringify(pets))
 	}
-
-
 });
 
 server.listen(PORT, function() {
 	console.log("Server is listening")
 })
-
 
 module.exports = server;
