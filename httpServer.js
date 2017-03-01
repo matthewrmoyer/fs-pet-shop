61a0f9149dce950b81c77ca061b6215039615866
Matthew Moyer <matthewrmoyer@gmail.com>
2017-03-01 14:54:09 -0700

    setup basic routes

--- httpServer.js
+++ httpServer.js
@@ -1,15 +1,29 @@
-var http = require('http')
+const http = require('http')
 const PORT = 8080
-
+const pets = require('./pets.json')
 
 
 var server = http.createServer(function(req, res) {
+	const index = req.url.split("/").pop();
+	console.log("Server is listening at " + index)
 	res.writeHead(200, {
-		"Content-Type": "text/html"
-	});
-	res.write("Hello World!");
-	res.end();
-})
+			"Content-Type": "application/json"
+		})
+		// res.end(JSON.stringify(pets));
+		// 		res.end(JSON.stringify(pets[0]));
+	if (index == "pets") {
+		res.end(JSON.stringify(pets));
+
+	} else if (index == "1") {
+		res.end(JSON.stringify(pets[1]));
+	} else if (index == "0") {
+		res.end(JSON.stringify(pets[0]))
+	} else{
+		res.end(JSON.stringify(pets))
+	}
+
+
+});
 
 server.listen(PORT, function() {
 	console.log("Server is listening")
@@ -17,4 +31,3 @@ server.listen(PORT, function() {
 
 
 module.exports = server;
-
