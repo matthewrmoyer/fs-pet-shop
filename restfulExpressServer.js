4aa6c8fc4ac296ebebd3ff8c835b7cf45efaf3e9
Matthew Moyer <matthewrmoyer@gmail.com>
2017-03-02 15:14:28 -0700

    post works so f it

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const low = require('lowdb')
+const fileAsync = require('lowdb/lib/storages/file-async')
+const db = low('./pets.json', {
+	storage: fileAsync
+})
 const path = require('path')
 const pets = require('./pets.json')
 const PORT = process.env.PORT || 8080
@@ -34,6 +39,57 @@ app.get('/pets/:id', (req, res) => {
 		res.set('Content-Type', 'application/json')
 		res.send(JSON.stringify(pets[id]))
 	}
+})
+
+app.post('/pets', (req, res) => {
+	var newPet = req.body
+		// res.set('Content-Type', 'application/json')
+		// res.status(200)
+	if (!newPet.age || !newPet.kind || !newPet.name) {
+		res.set('Content-Type', 'text/plain')
+		res.status(400)
+		res.send("Bad Request")
+		res.end()
+	} else {
+
+		var age = parseInt(newPet.age, 10)
+		var kind = newPet.kind
+		var name = newPet.name
+
+		var newPetObject = {age: age, kind: kind, name: name}
+		db.identity()
+			.push(newPetObject)
+			.write()
+			.then(function() {
+				res.set('Content-Type', 'application/json')
+				res.status(200)
+				res.send(newPetObject)
+			})
+			.catch(err => {
+				console.log(err)
+				res.set('Content-Type', 'text/plain')
+				res.status(400)
+				res.end("Bad Request")
+			})
+	}
+	// res.send(newPet)
+
+})
+
+app.delete('/pets/:id', (req, res) => {
+	var indexToDelete  = req.params.id -1;
+	console.log("deletingggg")
+	db.identity()
+		.remove(pets[indexToDelete])
+		.write()
+		.then(function(){
+		res.set('Content-Type', 'application/json')
+				res.status(200)
+				res.send(db)
+		})
+		.catch(err => {
+			console.log(err)
+		})
 })
 
 module.exports = app
